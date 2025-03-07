c5e80d8b15a8b247333b9a9e28ae083abf0fda0a
Sch <junhg0211@gmail.com>
2025-03-07 23:52:38 +0000

    debug contentify

--- contentify.py
+++ contentify.py
@@ -1,5 +1,8 @@
+from os import mkdir
+
 from obsidian_to_hugo import ObsidianToHugo
 
+mkdir('./content.')
 obsidian_to_hugo = ObsidianToHugo(
     obsidian_vault_dir="./wiki/",
     hugo_content_dir="./content",
