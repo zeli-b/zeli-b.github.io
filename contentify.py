f9fdaed5b3db632e57a54d7e8600ab67b10ad28f
Sch <junhg0211@gmail.com>
2025-03-07 23:53:45 +0000

    fix typo

--- contentify.py
+++ contentify.py
@@ -2,7 +2,7 @@ from os import mkdir
 
 from obsidian_to_hugo import ObsidianToHugo
 
-mkdir('./content.')
+mkdir('./content')
 obsidian_to_hugo = ObsidianToHugo(
     obsidian_vault_dir="./wiki/",
     hugo_content_dir="./content",
