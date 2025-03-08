996a65c4091e27c83de562587b2ca197b4325971
Sch <junhg0211@gmail.com>
2025-03-08 02:25:15 +0000

    enhance contentify

--- contentify.py
+++ contentify.py
@@ -1,11 +1,52 @@
-from os import mkdir
+from os import mkdir, walk
+from os.path import join, isdir
+from shutil import copy, rmtree
 
 from obsidian_to_hugo import ObsidianToHugo
 
-mkdir('./content')
-obsidian_to_hugo = ObsidianToHugo(
-    obsidian_vault_dir="./wiki/",
-    hugo_content_dir="./content",
-)
 
-obsidian_to_hugo.run()
+def addfrontmatter(wfile):
+    print(wfile)
+
+
+def main():
+    contentdir = './content'
+    wikidir = './wiki'
+    tmpdir = './tmp'
+
+    for path, directories, filenames in walk(wikidir):
+        tpath = join(tmpdir, path[7:])
+        isdir(tpath) and rmtree(tpath)
+        mkdir(tpath)
+        print(tpath)
+
+        if '_index.md' in filenames:
+            copy(wfile := join(path, '_index.md'), join(tpath, '_index.md'))
+            addfrontmatter(wfile)
+
+        for filename in filenames:
+            if not filename.endswith('.md'):
+                continue
+
+            if filename != '_index.md':
+                fpath = join(tpath, filename[:-3])
+
+                mkdir(fpath)
+                print(fpath)
+
+                copy(wfile := join(path, filename), join(fpath, '_index.md'))
+                addfrontmatter(wfile)
+
+
+    rmtree(contentdir)
+    mkdir(contentdir)
+    obsidian_to_hugo = ObsidianToHugo(
+        obsidian_vault_dir=tmpdir,
+        hugo_content_dir=contentdir,
+    )
+
+    obsidian_to_hugo.run()
+
+
+if __name__ == '__main__':
+    main()
